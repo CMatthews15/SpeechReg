18d2fc478ba55f94f85edafc1167bb42a424af4e
cmatthews15 <chastitydmatthews@gmail.com>
2023-10-26 14:58:38 -0400

    done

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const recognition = new SpeechRecognition();
 
 recognition.interimResults= true;
 recognition.lang = 'en-US';
+
+const words = document.querySelector(".words");
+let p = document.createElement('p');
+words.append(p);
+
+recognition.addEventListener('result', e =>{
+const transcript = Array.from (e.results)
+.map(result => result[0])
+.map(result => result.transcript)
+.join(' ');
+p.textContent=transcript;
+
+
+if(e.results[0].isFinal){
+    p = document.createElement('p');
+    words.appendChild(p);
+}
+});
+recognition.addEventListener('end',recognition.start);
+
+recognition.start()
+
+
